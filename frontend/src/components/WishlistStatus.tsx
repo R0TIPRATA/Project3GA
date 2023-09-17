f175b3513941046f25b1aea6133ee3ebba162943
nadiahafiqah <nadiaha.ismail@gmail.com>
2023-09-18 00:54:13 +0800

    days left is working now

--- frontend/src/components/WishlistStatus.tsx
+++ frontend/src/components/WishlistStatus.tsx
@@ -1,4 +1,16 @@
+import { useWishList } from "./context/WishlistContext";
+import { DateTime, Interval } from "luxon";
+
 const WishlistStatus = () => {
+  const { wishlist } = useWishList();
+
+  const daysLeft = (date: string) => {
+    const later = DateTime.fromISO(date);
+    const now = DateTime.now();
+    const i = Interval.fromDateTimes(now, later);
+    return parseInt(i.length("days"));
+  };
+
   return (
     <div className="w-96 mt-[15px] flex flex-row justify-between">
       {/* <!-- First Rectangle --> */}
@@ -7,7 +19,7 @@ const WishlistStatus = () => {
           DAYS LEFT
         </div>
         <div className="w-24 h-8 text-right text-black text-xl font-semibold  ml-[70px] mr-[15px] mt-[10px]">
-          30
+          {daysLeft(wishlist.campaignDate)}
         </div>
         <div className="w-5 h-5 left-[14px] top-[15px] absolute bg-zinc-300 rounded-full" />
       </div>
@@ -35,36 +47,6 @@ const WishlistStatus = () => {
         <div className="w-5 h-5 left-[14px] top-[15px] absolute bg-zinc-300 rounded-full" />
       </div>
     </div>
-
-    // <div className="w-64 h-24 relative flex flex-row justify-between">
-    //   <div className="order-1 w-64 h-24 left-0 top-0 absolute bg-white rounded-2xl" />
-    //   <div className="left-[160px] top-[18px] absolute text-right text-black text-sm font-light font-['Inter']">
-    //     DAYS LEFT
-    //   </div>
-    //   <div className="w-24 h-8 left-[142px] top-[48px] absolute text-right text-black text-xl font-semibold font-['Inter']">
-    //     30
-    //   </div>
-    //   <div className="w-5 h-5 left-[14px] top-[15px] absolute bg-zinc-300 rounded-full" />
-
-    //   <div className="order-2 w-64 h-24 left-0 top-0 absolute bg-white rounded-2xl" />
-    //   <div className="left-[153px] top-[18px] absolute text-right text-black text-sm font-light font-['Inter']">
-    //     ITEMS LEFT
-    //   </div>
-    //   <div className="w-24 h-8 left-[142px] top-[48px] absolute text-right text-black text-xl font-semibold font-['Inter']">
-    //     2
-    //   </div>
-    //   <div className="w-5 h-5 left-[14px] top-[15px] absolute bg-zinc-300 rounded-full" />
-
-    //   <div className="order-3 w-64 h-24 left-0 top-0 absolute bg-white rounded-2xl" />
-    //   <div className="left-[127px] top-[9px] absolute text-right text-black text-sm font-light font-['Inter']">
-    //     TOTAL <br />
-    //     CONTRIBUTION
-    //   </div>
-    //   <div className="w-24 h-8 left-[142px] top-[48px] absolute text-right text-black text-xl font-semibold font-['Inter']">
-    //     $120.00
-    //   </div>
-    //   <div className="w-5 h-5 left-[14px] top-[15px] absolute bg-zinc-300 rounded-full" />
-    // </div>
   );
 };
 
