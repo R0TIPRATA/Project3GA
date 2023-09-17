33e6a4761704eaa01bf7a4dec3da3a7c03a7a172
hzhec93 <chzhee@gmail.com>
2023-09-17 14:02:22 +0800

    add: welcome, user to navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -16,6 +16,7 @@ const Navbar = () => {
         </div>
         <div className="flex-none">
             <ul className="menu menu-horizontal px-1">
+            {userToken.username && <li><a className="text-white">Welcome, {userToken.username}!</a></li>}
             <li><a className="text-white">Wishlist</a></li>
             <li><a className="text-white">Recent Activity</a></li>
             {userToken.token && <li><a className="text-white" onClick={logoutHandler}>Logout</a></li>}
