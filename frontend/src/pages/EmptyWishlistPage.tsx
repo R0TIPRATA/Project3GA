8bf0cbe26a8899de05f803efe2820f16a1fa02a3
hzhec93 <chzhee@gmail.com>
2023-09-17 14:43:22 +0800

    update: emptywishlist page

--- frontend/src/pages/EmptyWishlistPage.tsx
+++ frontend/src/pages/EmptyWishlistPage.tsx
@@ -1,9 +1,16 @@
 const EmptyWishlistPage = () => {
   return (
     <div>
-        {/* insert placeholder image */}
-        <p>You don't have any wishlists yet! Add a wishlist to begin.</p>
-        <button>Add wishlist</button>
+      <div className="drawer drawer-end">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content h-whole bg-orange-100">
+            <div className="flex-col text-center my-80">
+              <div className="w-48 h-48 bg-gray-200 rounded-full mx-auto" />
+              <div className="my-6">You don't have any wishlists yet! Add a wishlist to begin.</div>
+              <button className="btn btn-primary">Add wishlist</button>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
