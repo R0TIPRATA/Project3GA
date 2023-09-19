cfb7b3e9cb46a519369129d4b8b6fcac2d77c13d
nadiahafiqah <nadiaha.ismail@gmail.com>
2023-09-19 21:54:55 +0800

    added haozhe contributor page

--- frontend/src/components/WishlistStatus.tsx
+++ frontend/src/components/WishlistStatus.tsx
@@ -1,5 +1,8 @@
 import { useWishList } from "./context/WishlistContext";
 import { DateTime, Interval } from "luxon";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDollarSign, faGift } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar } from "@fortawesome/free-regular-svg-icons";
 
 const WishlistStatus = () => {
   const { wishlist } = useWishList();
@@ -21,6 +24,7 @@ const WishlistStatus = () => {
 
 
   return (
+<<<<<<< Updated upstream
     <div className="w-1/2 mt-[15px] flex flex-row justify-between">
       {/* <!-- First Rectangle --> */}
       <div className="w-[200px] h-24 bg-white rounded-2xl">
@@ -45,10 +49,41 @@ const WishlistStatus = () => {
         </div>
         <div className="w-24 h-8 text-right text-black text-xl font-semibold  ml-[70px] mr-[20px] mt-[10px]">
           {getNumItems()}
+=======
+    <div className="w-[630px] mt-[15px] flex flex-row justify-between">
+      {/* <!-- First Rectangle --> */}
+      <div className="order-2 w-[200px] h-24 bg-white rounded-2xl flex justify-content">
+        <div className="icon m-4 w-1/4">
+          <FontAwesomeIcon icon={faCalendar} />{" "}
+        </div>
+        <div className="content m-4 w-3/4">
+          <div className="text-right text-black text-sm font-light pb-2">
+            DAYS LEFT
+          </div>
+          <div className="text-right text-black text-xl font-semibold">
+            {daysLeft(wishlist.campaignDate)}
+          </div>
+        </div>
+      </div>
+
+      {/* <!-- Second Rectangle --> */}
+      <div className="order-2 w-[200px] h-24 bg-white rounded-2xl flex justify-content">
+        <div className="icon m-4 w-1/4">
+          <FontAwesomeIcon icon={faGift} />
+        </div>
+        <div className="content m-4 w-3/4">
+          <div className="text-right text-black text-sm font-light pb-2">
+            ITEMS LEFT
+          </div>
+          <div className="text-right text-black text-xl font-semibold">
+            {getNumItems()}
+          </div>
+>>>>>>> Stashed changes
         </div>
       </div>
 
       {/* <!-- Third Rectangle --> */}
+<<<<<<< Updated upstream
       <div className="w-[250px] h-24 bg-white rounded-2xl">
         <div className="w-5 h-5 relative left-[14px] top-[15px] absolute bg-zinc-300 rounded-full" />
         <div className="text-right text-black text-sm font-light mr-[20px] mt-[18px]">
@@ -56,6 +91,19 @@ const WishlistStatus = () => {
         </div>
         <div className=" text-right text-black text-xl font-semibold ml-[70px] mr-[20px] mt-[10px]">
           $120.00
+=======
+      <div className="order-3 w-[200px] h-24 bg-white rounded-2xl flex justify-content">
+        <div className="icon m-4 w-1/4">
+          <FontAwesomeIcon icon={faDollarSign} />
+        </div>
+        <div className="content m-4 w-3/4">
+          <div className="text-right text-black text-sm font-light pb-2">
+            TOTAL GIFTED
+          </div>
+          <div className=" text-right text-black text-xl font-semibold">
+            $120.00
+          </div>
+>>>>>>> Stashed changes
         </div>
       </div>
     </div>
