e8e2d8a87a857e4909d3f1f4d981739fb8bfa29f
hzhec93 <chzhee@gmail.com>
2023-09-22 02:11:37 +0800

    changed 'payment' to 'contribution'

---
+++ server/routes/ContributionRouter.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const contributionCtrl = require("../controllers/contribution");
+
+router.get("/", contributionCtrl.getPublishableKey);
+router.post("/createstripepayment", contributionCtrl.createStripePayment);
+router.put("/updatestripepayment", contributionCtrl.updateStripePayment);
+router.get("/confirmstripepayment", contributionCtrl.confirmStripePayment);
+router.post("/addcontribution/:itemUuid", contributionCtrl.addContribution);
+
+module.exports = router;
