b39c0e20010d4e5dada4df5f6d47a21d30609824
nadiahafiqah <nadiaha.ismail@gmail.com>
2023-09-23 13:13:47 +0800

    fix navbar to only show wishlist and recent activity when logged in

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-// import { useNavigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useWishList } from "./context/WishlistContext";
 
@@ -8,25 +7,47 @@ const Navbar = () => {
 
   const logoutHandler = () => {
     localStorage.clear();
-    navigate('/login');
+    navigate("/login");
     window.location.reload();
-  }
+  };
 
   return (
     <div className="navbar bg-forest text-white">
-        <div className="flex-1">
-            <a className="text-white normal-case text-xl ml-10 pointer-events-none">Wishlist App</a>
-        </div>
-        <div className="flex-none">
-            <ul className="menu menu-horizontal px-1">
-            {userToken.username && <li><a className="text-white pointer-events-none">Welcome, {userToken.username}!</a></li>}
-            <li><a className="text-white">Wishlist</a></li>
-            <li><a className="text-white">Recent Activity</a></li>
-            {userToken.token && <li><a className="text-white" onClick={logoutHandler}>Logout</a></li>}
-            </ul>
-        </div>
+      <div className="flex-1">
+        <a className="text-white normal-case text-xl ml-10 pointer-events-none">
+          Wishlist App
+        </a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          {userToken.username && (
+            <li>
+              <a className="text-white pointer-events-none">
+                Welcome, {userToken.username}!
+              </a>
+            </li>
+          )}
+          {userToken.token && (
+            <li>
+              <a className="text-white">Wishlist</a>
+            </li>
+          )}
+          {userToken.token && (
+            <li>
+              <a className="text-white">Recent Activity</a>
+            </li>
+          )}
+          {userToken.token && (
+            <li>
+              <a className="text-white" onClick={logoutHandler}>
+                Logout
+              </a>
+            </li>
+          )}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/src/components/contributor/LoggedInNotif.tsx
+++ frontend/src/components/contributor/LoggedInNotif.tsx
@@ -8,7 +8,7 @@ const LoggedInNotif = () => {
       {userToken.token && (
         <div className="h-12 bg-zinc-300 flex justify-center items-center">
           This is what your friends/family see when they want to contribute to
-          your page. To edit your wishlist,{" "}
+          your page. To edit your wishlist,
           <a className="pl-1 underline">click here</a>.
         </div>
       )}
