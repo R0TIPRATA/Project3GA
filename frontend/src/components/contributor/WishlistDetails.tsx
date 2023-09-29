0a13a133586383b94eba25b1fdebfdc94b0ccde3
aliyah <45678974+R0TIPRATA@users.noreply.github.com>
2023-09-29 20:57:08 +0800

    edit url

--- frontend/src/components/contributor/WishlistDetails.tsx
+++ frontend/src/components/contributor/WishlistDetails.tsx
@@ -14,7 +14,7 @@ const WishListDetails = () => {
   };
 
   const copyLink = () => {
-    const listLink = `${import.meta.env.VITE_APP_FRONTEND_URL}/wishlist/${user}`;
+    const listLink = `${import.meta.env.VITE_APP_API_URL}/wishlist/${user}`;
     navigator.clipboard.writeText(listLink);
     console.log(listLink);
     console.log("copied");
